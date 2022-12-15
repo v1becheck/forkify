bed52f7026979546741b64c9b6d0e73e37ad731e
Milos Ilic <milosilic89@outlook.com>
2022-12-15 09:11:16 +0100

    Refactoring for MVC

---
+++ src/js/model.js
@@ -0,0 +1,31 @@
+import 'regenerator-runtime/runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      title: recipe.title,
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
