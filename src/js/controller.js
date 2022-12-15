abe5592f1b6d47ad06b5f55eafdb078346f51b1c
Milos Ilic <milosilic89@outlook.com>
2022-12-15 14:44:04 +0100

    Adding Event Handler to MVC: Publisher-Subscriber

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,8 +28,9 @@ const controlRecipes = async function () {
   }
 };
 
-controlRecipes();
+// controlRecipes();
 
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
