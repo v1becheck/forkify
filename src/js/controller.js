bc77d82b48c8d0d1bd30161c61b630780af79eb5
Milos Ilic <milosilic89@outlook.com>
2022-12-17 11:30:37 +0100

    Implementing Pagination Buttons Functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,8 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-import ResultsView from './views/resultsView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -44,14 +45,26 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //Render results
-    resultsView.render(model.getSearchResultsPage(2));
+    resultsView.render(model.getSearchResultsPage(1));
+
+    //Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  //Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
